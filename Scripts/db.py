a6c9775e36a2267d990c1d1395250cc0cec8fd9f
Salai-Kowshikan <salaikowshikan8743@gmail.com>
2024-07-15 17:19:23 +0530

    Server refactoring

--- Scripts/db.py
+++ Scripts/db.py
@@ -1,21 +1,21 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".dev.env")
 
 db = None
 
-uri = "mongodb+srv://ezhildhiraviya:6x2iLR4Y9j02fT1c@face-recognition.n1qkn6p.mongodb.net/?appName=face-recognition"
+db_url = os.getenv("MONGODB_URI")
+print("string: ", db_url)
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(db_url, server_api=ServerApi('1'))
 
 db = client['face-db']
 
-# Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
-
-
-    # face-recognition
