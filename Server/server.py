3211b63020e288cc06c76de0437dfb971cec7e1e
Varun-Karthik-T <varunkarthikceg@gmail.com>
2024-07-22 21:06:47 +0530

    host change

--- Server/server.py
+++ Server/server.py
@@ -51,4 +51,5 @@ def upload_file():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.config['DEBUG'] = False
+    app.run(host='0.0.0.0', port=5000)
